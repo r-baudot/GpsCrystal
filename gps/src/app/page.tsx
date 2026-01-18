4b88c0bc0cb54d33ab215cf13a012f13067aa3a6
Romain Baudot <baudot.romain@gmail.com>
2026-01-18 18:25:05 +0100

    Feature WIP - init wizard

--- gps/src/app/page.tsx
+++ gps/src/app/page.tsx
@@ -1,19 +1,22 @@
-import Wizard from "../components/wizard/Wizard";
+import { Wizard } from "@/components/wizard/Wizard";
+import { Box } from "@mui/material";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the GPS Application</h1>
-      <br />
-      <ul>
-        1 - permettre la saisie de position GPS sous forme d'un wizard
-        (formulaire à étape) :
-      </ul>
-      <li>- Etape 1 : Saisie du nom</li>
-      <li>- Etape 2 : Saisie des coordonnées</li>
-      <li>- Etape 3 : Récapitulatif avant </li>
-      <p>TO DO : implémenter le wizard MUI (3 étapes) + setUp storyBook</p>
+    <>
+      <Box>
+        <h1>Welcome to the GPS Application</h1>
+        <br />
+        <ul>
+          1 - permettre la saisie de position GPS sous forme d'un wizard
+          (formulaire à étape) :
+        </ul>
+        <li>- Etape 1 : Saisie du nom</li>
+        <li>- Etape 2 : Saisie des coordonnées</li>
+        <li>- Etape 3 : Récapitulatif avant </li>
+        <p>TO DO : implémenter le wizard MUI (3 étapes) + setUp storyBook</p>
+      </Box>
       <Wizard />
-    </div>
+    </>
   );
 }
--- gps/src/components/wizard/Wizard.tsx
+++ gps/src/components/wizard/Wizard.tsx
@@ -1,5 +1,65 @@
-const Wizard = () => {
-  return <div>Wizard Component Placeholder</div>;
-};
+"use client";
+import MuiStepper from "@mui/material/Stepper";
+import Step from "@mui/material/Step";
+import StepLabel from "@mui/material/StepLabel";
+import Box from "@mui/material/Box";
+import { ReactNode } from "react";
+import { useState } from "react";
+import { Button } from "@mui/material";
 
-export default Wizard;
+export interface StepConfig {
+  label: string;
+  content: ReactNode;
+}
+
+export interface StepperProps {
+  steps: StepConfig[];
+  activeStep: number;
+}
+const STEPS = [
+  {
+    label: "Saisie du nom",
+    content: <div>Contenu de l'étape 1 : Saisie du nom</div>,
+  },
+  {
+    label: "Saisie des coordonnées",
+    content: <div>Contenu de l'étape 2 : Saisie des coordonnées</div>,
+  },
+  {
+    label: "Récapitulatif avant",
+    content: <div>Contenu de l'étape 3 : Récapitulatif avant</div>,
+  },
+];
+
+export const Wizard = () => {
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((activeStep) => activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((activeStep) => activeStep - 1);
+  };
+
+  return (
+    <Box>
+      <MuiStepper activeStep={activeStep} sx={{ mb: 3 }}>
+        {STEPS.map((step) => (
+          <Step key={step.label}>
+            <StepLabel>{step.label}</StepLabel>
+          </Step>
+        ))}
+      </MuiStepper>
+      <Box>{STEPS[activeStep].content}</Box>
+      <Box>
+        <Button onClick={handleBack} disabled={activeStep === 0}>
+          Précédent
+        </Button>
+        {activeStep !== STEPS.length - 1 && (
+          <Button onClick={handleNext}>Suivant</Button>
+        )}
+      </Box>
+    </Box>
+  );
+};
