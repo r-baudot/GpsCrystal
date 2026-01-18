1f046e9c5c0224c2fc27525d4feda201b0de6ca1
Romain Baudot <baudot.romain@gmail.com>
2026-01-18 18:01:14 +0100

    Clean project & add architecture folder

---
+++ gps/src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to the GPS Application</h1>
+      <br />
+      <ul>
+        1 - permettre la saisie de position GPS sous forme d'un wizard
+        (formulaire à étape) :
+      </ul>
+      <li>- Etape 1 : Saisie du nom</li>
+      <li>- Etape 2 : Saisie des coordonnées</li>
+      <li>- Etape 3 : Récapitulatif avant </li>
+    </div>
+  );
+}
